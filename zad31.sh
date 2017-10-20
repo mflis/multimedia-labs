523dd88a5da90c97f65e61717427653d9b41d9e5
Marcin Flis <marcin.flis.95@gmail.com>
2017-10-20 14:43:33 +0200

    Update zad31.sh

--- zad31.sh
+++ zad31.sh
@@ -3,7 +3,7 @@
 
 # $1 input file
 # $2 mowa, muzyka
-# $3 $4 $5 - quality 0-9
+# $3 $4 $5 - quality 0-9, ma być 0, 9 i coś pośrodku
 ffmpeg -i $1 -codec:a libmp3lame -qscale:a $3 zad31$2$3.mp3
 ffmpeg -i $1 -codec:a libmp3lame -qscale:a $4 zad31$2$4.mp3
 ffmpeg -i $1 -codec:a libmp3lame -qscale:a $5 zad31$2$5.mp3
